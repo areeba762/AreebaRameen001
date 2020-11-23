1c72d661f45bc211d2b98d8ea299f84eef15924b
areeba762 <74913951+areeba762@users.noreply.github.com>
2020-11-23 09:25:55 -0800

    Add files via upload

---
+++ Areeba.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package areeba;
+
+/**
+ *
+ * @author sawera bukhari
+ */
+
+
+class Node 
+{ 
+    int data; 
+    Node next; 
+  
+    public Node(int data) 
+    { 
+        this.data = data; 
+        this.next = null; 
+    } 
+} 
+
+class Queue
+{ 
+    Node front, rear; 
+  
+    public Queue() 
+    { 
+        front = rear = null; 
+    } 
+  
+    public void enqueue(int data) 
+    { 
+        Node temp = new Node(data); 
+        
+        if (rear == null)
+	{ 
+            front = rear = temp; 
+            return; 
+        } 
+         
+        rear.next = temp; 
+        rear = temp; 
+    } 
+    
+    void dequeue() 
+    { 
+        if (front == null) 
+	{
+            System.out.println("Empty!");
+	    return; 
+	}          
+  
+        Node temp = front; 
+        front = front.next; 
+         
+        if (front == null) 
+        {
+            rear = null; 
+        } 
+    }
+}
+  
+public class Areeba{ 
+    public static void main(String[] args) 
+    { 
+        Queue q = new Queue(); 
+       
+        q.dequeue();
+        q.enqueue(10); 
+        q.enqueue(20); 
+        q.dequeue(); 
+        q.dequeue(); 
+
+        q.enqueue(30); 
+        q.enqueue(40); 
+        q.enqueue(50); 
+        q.dequeue(); 
+    } 
+}
+
+
